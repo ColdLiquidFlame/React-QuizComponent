69b2bffd3e9b96bfc94411c5a0d837fe260b9efb
Chris White <chris.white@capco.com>
2019-04-24 13:46:36 -0500

    Step 1.1

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,9 +3,9 @@ let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
     constructor(props){
-        super(props);
+        super(props)
 
-        this.state = {quiz_position: 1};
+        this.state = {quiz_position: 1}
     }
 
     render = () => {

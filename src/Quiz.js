be74da47cb8e401fcc6ce7595f5fbe76693503cc
Chris White <chris.white@capco.com>
2019-04-24 13:47:42 -0500

    Step 1.2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-let quizData = require('./quiz_data.json');
+import React, { Component } from 'react'
+let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
     constructor(props){
         super(props)
-
         this.state = {quiz_position: 1}
     }
 
